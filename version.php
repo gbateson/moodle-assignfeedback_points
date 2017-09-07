cf407384beed25d3093f1770d1a8d4ee30c2f701
Gordon Bateson <gordonbateson@gmail.com>
2017-09-07 22:39:12 +0800

    assignfeedback_points fix error regarding 'undefined method' get_all_user_name_fields in db/upgrade.php

--- version.php
+++ version.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'assignfeedback_points';
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2017092758;
-$plugin->release   = '2017-09-27 (58)';
+$plugin->version   = 2017090759;
+$plugin->release   = '2017-09-07 (59)';
